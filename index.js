ec39862dfbfe09fdd77fb935ff67382d98c6ffc5
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-24 01:01:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return Object.assign (object, {[key]: value})
 }
+
+function deleteFromObjectByKey(object, key) {
+  object = newObj(object, key)
+  delete newObj[key]
+  return newObj
+}
